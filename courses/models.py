3dcdfe59ffa137432f5d6e37698623fb8785be88
Otabek <otabekrahmonovtest@gmail.com>
2022-08-16 10:40:04 +0500

    Modellar uchun qo`shimcha metodlar

--- courses/models.py
+++ courses/models.py
@@ -1,12 +1,21 @@
 from django.conf import settings
 from django.db import models
 import uuid 
+from django.contrib.auth import get_user_model
+from decimal import Decimal
+from .helpers import get_timer
+
+from mutagen.mp4 import MP4, MP4StreamInfoError
 
 class Sector(models.Model):
     name = models.CharField(max_length=255)
     sector_uuid = models.UUIDField(default=uuid.uuid4, unique=True)
     releted_course = models.ManyToManyField('Course')
     sector_image = models.ImageField(upload_to='sector_image')
+
+    # /media/sector_image/rasm.png
+    def get_image_absolute_url(self):
+        return 'http://localhost:8000'+self.sector_image
 
 class Course(models.Model):
     title = models.CharField(max_length=255)
@@ -21,16 +30,58 @@ class Course(models.Model):
     course_uuid = models.UUIDField(default=uuid.uuid4, unique=True)
     price = models.DecimalField(max_digits=5, decimal_places=2)
 
+    def get_brief_description(self):
+        return self.description[:100]
+
+    def get_enrolled_student(self):
+        students = get_user_model().objects.filter(paid_courses=self)
+        return len(students)
+
+    def get_total_lectures(self):
+        lectures = 0
+        for section in self.course_section:
+            lectures += len(section.episode.all())
+        return lectures
+
+    def total_course_length(self):
+        length = Decimal(0.0)
+        for section in self.course_section:
+            for episode in section.episode.all():
+                length += episode.length
+        return get_timer(length, type='short')
 
 class CourseSection(models.Model):
     section_title = models.CharField(max_length=255)
     episodes = models.ManyToManyField('Episode')
+
+    def total_length(self):
+        total = Decimal(0.0)
+        for episode in self.episodes.all():
+            total+=episode.length
+        return get_timer(total, type='min')
 
 
 class Episode(models.Model):
     title = models.CharField(max_length=255)
     file = models.FileField(upload_to='course_videos')
     length = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def get_video_length(self):
+        try:
+            video=MP4(self.file)
+            return video.info.length
+        except MP4StreamInfoError:
+            return 0.0
+    
+    def get_video_length_time(self):
+        return get_timer(self.length)
+
+    def get_absolute_url(self):
+        return 'http://localhost:8000'+self.file
+
+    def save(self, *args, **kwargs):
+        self.length=self.get_video_length()
+        return super().save(*args, **kwargs)
 
 
 class Comment(models.Model):
